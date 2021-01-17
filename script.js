6547ac3ac07debda580cdad0c11ecb24bed91f8d
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-17 22:32:40 +0100

    Completamento esercizio, inserito prompt nel ciclo d-while e risultati

--- script.js
+++ script.js
@@ -40,3 +40,57 @@ console.log(numeriRandom);
 setTimeout(function() {
     $('#numeri').text(''); // '' per togliere i numeri alla fine del CD
 }, 30000);
+
+
+// dopo aver nascosto i numeri, chiedo all'utente di inserire 5 numeri
+// e controllo quali indovina tra quelli generati precedentemente
+setTimeout(function() {
+    // creo un array per ricordarmi tutti i numeri che ha inserito l'utente
+    var numeriInseriti = [];
+    // creo un contatore per il punteggio
+    var punteggio = 0;
+    // creo un array per ricordarmi i numeri indovinati
+    var numeriIndovinati = [];
+
+    // ciclo do-while per chiedere i numeri all'utente
+    // in questo modo possiamo controllare che ogni volta siano numeri diversi
+    // il ciclo ripete finché l'utente non ha inserito 5 numeri (tutti diversi)
+    do {
+        var numeroUtente = parseInt(prompt('inserisci un numero visualizzato prima'));
+        console.log(numeroUtente);
+
+        // controllo se l'utente ha inserito un numero nuovo
+        if(!numeriInseriti.includes(numeroUtente))  {
+            // l'utente ha inserito un numero diverso dai precedenti
+            // inserisco il numero tra i numeri inseriti
+            numeriInseriti.push(numeroUtente);
+
+            // controllo se il numero inserito corrisponde ad uno dei numeri generati precedentemente
+            if(numeriRandom.includes(numeroUtente)) {
+                // l'utente ha indovinato un numero
+                // incremento di uno il punteggio
+                punteggio++;
+                // inserisco il numero tra i numeri indovinati
+                numeriIndovinati.push(numeroUtente);
+            }
+        }
+    } while (numeriInseriti.length < 5);
+
+    // visualizzo nuovamente i numeri casuali
+    var numeriStringa = numeriRandom.join(' ');
+    $('#numeri').text(numeriStringa);
+
+    // visualizzo i numeri inseriri dall'utente
+    var numeriUtenteStringa = numeriInseriti.join(' ');
+    $('#numeriUtente').text(numeriUtenteStringa);
+
+    // visualizzo i numeri indovinati
+    var numeriIndovinatiStringa = numeriIndovinati.join(' ');
+    $('#numeriIndovinati').text(numeriIndovinatiStringa);
+
+    console.log(numeriInseriti);
+    console.log(numeriIndovinati);
+    console.log(punteggio);
+    alert('hai totalizzato ' + punteggio + ' punti');
+
+}, 31000);
